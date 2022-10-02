fcf8c36353be0e221a865e79cbe816ede14e715c
Sarah Foster <fostersarah@ufl.edu>
2022-10-01 21:52:06 -0400

    Finished density plot and edited Assignment2 r file

--- Assignment2.R
+++ Assignment2.R
@@ -1,46 +1,45 @@
 library("readxl")
 library("dplyr")
 library("ggplot2")
-#read in all 6 tables
-#my_data1 <- read_excel("./FPKM_Data/Data1.xlsx")
-#View(my_data1)
+library("matrixStats")
 
-#my_data2 <- read_excel("./FPKM_Data/Data2.xlsx")
-#View(my_data2)
+#read in all 4 tables
 
 my_data3 <- read_excel("./FPKM_Data/Data3.xlsx")
-View(my_data3)
+#View(my_data3)
 
 my_data4 <- read_excel("./FPKM_Data/Data4.xlsx")
-View(my_data4)
+#View(my_data4)
 
 my_data5 <- read_excel("./FPKM_Data/Data5.xlsx")
-View(my_data5)
+#View(my_data5)
 
 my_data6 <- read_excel("./FPKM_Data/Data6.xlsx")
-View(my_data6)
+#View(my_data6)
 
-#merge all of the A, B and C tables in their own 3 tables
-#tableA <- union_all(my_data1,my_data2)
-#View(tableA)
+#merge B and C tables in their own tables
 
 table_b <- union_all(my_data6,my_data4)
-View(table_c)
+#View(table_c)
 
 table_c <- union_all(my_data5,my_data3)
-View(table_c)
+#View(table_c)
 
-#issues: the three tables don't seem to have any genes in common 
-#not sure what to do next
-#mergeAB <- union_all(tableA, tableB)
-#View(mergeAB)
-
+#merge B and C into a single expression matrix
 merge_all <- union_all(table_b, table_c)
-View(merge_all)
+#View(merge_all)
 
 expression_matrix <- merge_all[!duplicated(merge_all$gene_id), ]
 View(expression_matrix)
 
-density_plot_data <- read_excel("./DensityPlotData.xlsx")
-View(density_plot_data)
-ggplot(density_plot_data, aes(log2(x = x), fill = Sample)) + geom_density(alpha = .2)
+#replace all n/a values with 0
+expression_matrix[is.na(expression_matrix)] = 0
+#finds the max values in each row(gene) and adds it to the end of expression_matrix
+expression_matrix$row_maximum = rowMaxs(as.matrix(expression_matrix[,c(-1)]))
+expression_matrix
+
+#creates a data frame of only the max values
+row_range <- expression_matrix[ , ncol(expression_matrix), drop = FALSE]
+
+#plotting the density 
+ggplot(row_range, aes(log2(x = row_maximum))) + geom_density(alpha = .2) +ggtitle("Density Plot")
